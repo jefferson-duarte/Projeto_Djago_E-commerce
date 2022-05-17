639d409cc33b29a6d20aef1b495de785cd72dd00
Jefferson Duarte <jeffersonsduarte@live.com>
2022-05-17 13:09:35 +0100

    Carrinho produto renderizado

--- produto/templatetags/jdfilters.py
+++ produto/templatetags/jdfilters.py
@@ -1,5 +1,5 @@
 from django.template import Library
-from utils import utils 
+from utils import utils
 
 register = Library()
 
@@ -7,3 +7,8 @@ register = Library()
 @register.filter
 def formata_preco(val):
     return utils.formata_preco(val)
+
+
+@register.filter
+def cart_total_qtd(carrinho):
+    return utils.cart_total_qtd(carrinho)
